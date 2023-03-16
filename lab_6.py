0d0192a80a4eb7373c84cae80ef298f06ecf7a51
lilypad320 <127242703+lilypad320@users.noreply.github.com>
2023-03-16 15:44:53 -0400

    Add files via upload

---
+++ lab_6.py
@@ -0,0 +1,20 @@
+run = True
+while run:
+    print('Menu\n-------------\n1.Encode\n2.Decode\n3.Quit')
+    user_input = int(input())
+    if user_input == 1:
+        print('Please enter your password to encode:', end=' ')
+        password_string = input()
+        encoded_list = [int(i) for i in str(password_string)]
+        print(encoded_list)
+        new_list = []
+        for num in encoded_list:
+            new_list.append(num + 3)
+        encoded_string = [str(i) for i in new_list]
+        encoded_list = ''.join(encoded_string)
+        print('Your password has been encoded and stored!')
+    elif user_input == 2:
+        pass
+    elif user_input == 3:
+        run = False
+        break
