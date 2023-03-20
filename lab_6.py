4340ae4829b97d28b0c951b3af58da7302df32e8
lilypad320 <lizr3225@gmail.com>
2023-03-20 14:20:41 -0400

    added decode function

--- lab_6.py
+++ lab_6.py
@@ -14,7 +14,15 @@ while run:
         encoded_list = ''.join(encoded_string)
         print('Your password has been encoded and stored!')
     elif user_input == 2:
-        pass
+        encoded_string = int(input())
+        decode_list = [int(i) for i in str(encoded_string)]
+        new_list = []
+        for i in decode_list:
+            i -= 3
+            new_list.append(i)
+        new_string = [str(i) for i in new_list]
+        decode_string = ''.join(new_string)
+        print(f'The encoded password is {encoded_string}, and the original password is {decode_string}.')
     elif user_input == 3:
         run = False
         break
